c348237cdc1df71133466e60fe799130179b4b3f
ALOK SINGH <123860933+2004-AlokSINGH@users.noreply.github.com>
2025-12-14 17:21:28 +0530

    Added sequential runnable file

---
+++ runnables/runnableSeq.py
@@ -0,0 +1,55 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain_core.runnables import Runnable
+from langchain_huggingface import ChatHuggingFace, HuggingFacePipeline
+import os
+from langchain_huggingface import HuggingFacePipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import torch
+
+
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+# model = ChatOpenAI() 
+# If we donnt have chatOpenAI cost then so am using free models
+
+
+model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16 if torch.cuda.is_available() else torch.float32,
+   
+)
+
+llm_pipeline = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+    max_new_tokens=100,
+    temperature=0.5
+)
+
+model = HuggingFacePipeline(pipeline=llm_pipeline)
+
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+# chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+chain= prompt1 | model | parser | prompt2 | model | parser
+
+print(chain.invoke({'topic':'AI'}))
